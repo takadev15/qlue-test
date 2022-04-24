6e0a4d98bf9e12d06b9afe834a68108620e9e241
Taka <daffa.hp.dh@gmail.com>
2022-04-24 23:31:09 +0700

    feat(test): finish 4th question

--- question-2/query/init.sql
+++ question-2/query/init.sql
@@ -1,8 +1,22 @@
 CREATE TABLE persons (
   id int NOT NULL
   name varchar(200)
-)
+  PRIMARY KEY(id)
+);
 
 CREATE TABLE items (
   id int NOT NULL
+  name varchar(200)
+  price varchar(10)
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE transactions (
+  id int NOT NULL
+  person_id int NOT NULL
+  item_id int NOT NULL
+  total int
+  date timestamp
+  FOREIGN KEY(person_id) REFERENCES persons(id)
+  FOREIGN KEY(item_id) REFERENCES items(id)
 );
--- question-4/app.js
+++ question-4/app.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
 
 const jsonInput = require('./input.json');
 const port = 3030;
+const result = {"h":[], "d":[[]]}
 
-for(var i = 0; i < jsonInput.length; i++) {
-  var obj = jsonInput[i]
-  console.log(obj.username)
-}
+result.h = Object.keys(jsonInput[0])
+result.d = jsonInput.map((obj) => {
+  return Object.values(obj)
+})
 
 app.get('/', (req, res) => {
-  res.send(jsonInput);
+  res.send(result);
 });
 
 app.listen(port, () => {
   console.log(`Web server running at port ${port}`);
 });
+
